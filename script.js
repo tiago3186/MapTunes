a1ea7277398841a92c3d8aa74ccd12803823e018
Tiago Luis Custódio <132753395+tiago3186@users.noreply.github.com>
2023-09-06 15:09:17 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -42,29 +42,60 @@ if (window.location.pathname === "/login.html") {
 document.addEventListener("DOMContentLoaded", () => {
     // Sua chave da API do Last.fm
     const apiKey = config.apiKey;
-    
+
     // Seu nome de usuário no Last.fm
     const lastfmusername = config.lastfmProfile;
 
-    console.log(apiKey, lastfmusername)
-    
+    const welcomeMessage2 = document.getElementById("welcomeMessage2");
+    const tableBody = document.querySelector("tbody");
+
+    welcomeMessage2.textContent = `Seu perfil do lastfm é ${lastfmusername}!`;
+
+    function fetchArtistTags(artistName, apiKey, limit=10) {
+        const apiUrl = `https://ws.audioscrobbler.com/2.0/?method=artist.gettoptags&artist=${artistName}&api_key=${apiKey}&format=json`;
+
+        return fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            // Verifique se há tags disponíveis
+            if (data.toptags && data.toptags.tag && data.toptags.tag.length > 0) {
+                const tags = data.toptags.tag.slice(0, limit).map(tag => tag.name).join(', ');
+                return tags;
+            } else {
+                return "Nenhuma tag disponível";
+            }
+        })
+            .catch(error => {
+                console.error(`Erro ao buscar tags para ${artistName}: ${error}`);
+                return "Erro ao buscar tags";
+            });
+    }
+
     // URL da API do Last.fm para obter os artistas mais ouvidos
     const apiUrl = `https://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=${lastfmusername}&api_key=${apiKey}&limit=10&format=json`;
-    
-    console.log(apiUrl)
-
-    const tableBody = document.querySelector("tbody");
 
     fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
-            console.log(data)
+            console.log(data);
             const topArtists = data.topartists.artist;
-            topArtists.forEach((artist, index) => {
+
+            const artistPromises = topArtists.map(artist => {
+                return fetchArtistTags(artist.name, apiKey)
+                    .then(tags => {
+                        artist.tags = tags;
+                        return artist;
+                    });
+            });
+
+            return Promise.all(artistPromises);
+        })
+        .then(topArtistsWithTags => {
+            topArtistsWithTags.forEach((artist, index) => {
                 const position = index + 1;
                 const name = artist.name;
                 const playcount = artist.playcount;
-                const tags = "Nenhuma tag disponível";
+                const tags = artist.tags;
                 const row = `<tr>
                     <td>${position}</td>
                     <td>${name}</td>

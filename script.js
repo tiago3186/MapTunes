754f0fb011e2555b71cf88df5253415d16fa7574
Tiago Luis Custódio <132753395+tiago3186@users.noreply.github.com>
2023-09-06 16:11:57 -0300

    FIX tramites de login

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ import config from './config.js';
 
 // Defina um objeto com os usernames e senhas permitidos
 const users = {
-    "admin": "admin",
+    "tiagoluis86": "Quadrado86?",
     // Adicione mais usernames e senhas aqui conforme necessário
 };
 
@@ -37,20 +37,24 @@ function logout() {
 if (window.location.pathname === "/login.html") {
     const loginForm = document.getElementById("loginForm");
     loginForm.addEventListener("submit", checkLogin);
-} else if (window.location.pathname === "/index.html") {
-    // Se estamos na página de boas-vindas (index.html), exiba a mensagem de boas-vindas
+} else {
+    // Verifica se o usuário está autenticado
     const username = localStorage.getItem("username");
-    if (username) {
-        const welcomeMessage = document.getElementById("welcomeMessage");
-        welcomeMessage.textContent = `Bem-vindo, ${username}!`;
-    } else {
-        // Se não houver um nome de usuário, redirecione de volta para a página de login
+    
+    if (!username) {
+        // Se o usuário não estiver autenticado, redirecione para a página de login
         window.location.href = "login.html";
     }
-
-    // Adicione um evento de clique ao botão de logout
-    const logoutButton = document.getElementById("logoutButton");
-    logoutButton.addEventListener("click", logout);
+    
+    if (window.location.pathname === "/index.html") {
+        // Se estamos na página de boas-vindas (index.html), exiba a mensagem de boas-vindas
+        const welcomeMessage = document.getElementById("welcomeMessage");
+        welcomeMessage.textContent = `Bem-vindo, ${username}!`;
+        
+        // Adicione um evento de clique ao botão de logout
+        const logoutButton = document.getElementById("logoutButton");
+        logoutButton.addEventListener("click", logout);
+    }
 }
 
 

ded041936c4eaa6acb54f54b8cff4b8a6a78d13f
Tiago Luis Custódio <132753395+tiago3186@users.noreply.github.com>
2023-09-06 14:30:16 -0300

    ADD separacao das variaveis de ambiente

---
+++ config.js
@@ -0,0 +1,8 @@
+// config.js
+
+const config = {
+    apiKey: "SUA_API_KEY_AQUI",
+    lastfmProfile: "SEU_PERFIL_LASTFM_AQUI"
+};
+
+export default config;
--- script.js
+++ script.js
@@ -1,55 +1,60 @@
+import config from './config.js';
+
+
+// Defina um objeto com os usernames e senhas permitidos
+const users = {
+    "admin": "admin",
+    // Adicione mais usernames e senhas aqui conforme necessário
+};
+
+// Função para verificar o login
+function checkLogin(event) {
+    event.preventDefault(); // Impede o envio do formulário
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    if (users[username] === password) {
+        // Se as credenciais estiverem corretas, redirecione para index.html
+        window.location.href = "index.html";
+    } else {
+        alert("Credenciais incorretas. Tente novamente.");
+    }
+}
+
+// Verifica se estamos na página de login
+if (window.location.pathname === "/login.html") {
+    const loginForm = document.getElementById("loginForm");
+    loginForm.addEventListener("submit", checkLogin);
+} else if (window.location.pathname === "/index.html") {
+    // Se estamos na página de boas-vindas (index.html), exiba a mensagem de boas-vindas
+    const username = localStorage.getItem("username");
+    if (username) {
+        const welcomeMessage = document.getElementById("welcomeMessage");
+        welcomeMessage.textContent = `Bem-vindo, ${username}!`;
+    } else {
+        // Se não houver um nome de usuário, redirecione de volta para a página de login
+        window.location.href = "login.html";
+    }
+}
+
+
 document.addEventListener("DOMContentLoaded", () => {
-    // Defina um objeto com os usernames e senhas permitidos
-    const users = {
-        "admin": "admin",
-        // Adicione mais usernames e senhas aqui conforme necessário
-    };
-    
-    // Função para verificar o login
-    function checkLogin(event) {
-        event.preventDefault(); // Impede o envio do formulário
-    
-        const username = document.getElementById("username").value;
-        const password = document.getElementById("password").value;
-    
-        if (users[username] === password) {
-            // Se as credenciais estiverem corretas, redirecione para index.html
-            localStorage.setItem("username", username); // Armazena o nome de usuário
-            window.location.href = "index.html";
-        } else {
-            alert("Credenciais incorretas. Tente novamente.");
-        }
-    }
-    
-    // Verifica se estamos na página de login
-    if (window.location.pathname === "/login.html") {
-        const loginForm = document.getElementById("loginForm");
-        loginForm.addEventListener("submit", checkLogin);
-    } else if (window.location.pathname === "/index.html") {
-        // Se estamos na página de boas-vindas (index.html), exiba a mensagem de boas-vindas
-        const username = localStorage.getItem("username");
-        if (username) {
-            const welcomeMessage = document.getElementById("welcomeMessage");
-            welcomeMessage.textContent = `Bem-vindo, ${username}!`;
-        } else {
-            // Se não houver um nome de usuário, redirecione de volta para a página de login
-            window.location.href = "login.html";
-        }
-    }
-    
     // Sua chave da API do Last.fm
-    const apiKey = "";
+    const apiKey = config.apiKey;
     
     // Seu nome de usuário no Last.fm
-    const username = "";
+    const lastfmusername = config.lastfmProfile;
+
+    console.log(apiKey, lastfmusername)
     
     // URL da API do Last.fm para obter os artistas mais ouvidos
-    const apiUrl = `https://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=${username}&api_key=${apiKey}&limit=10&format=json`;
+    const apiUrl = `https://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=${lastfmusername}&api_key=${apiKey}&limit=10&format=json`;
     
     console.log(apiUrl)
-    
+
     const tableBody = document.querySelector("tbody");
-    
+
     fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
